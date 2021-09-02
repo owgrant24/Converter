e572f99fad00e0773257f78fa7b53ea0096dbeb6
Aleksandr Shabelskii <owgrant18@gmail.com>
2021-09-02 15:27:35 +0300

    020921 bugfix actionRemoveSelectedFilesFromTable

--- src/main/java/com/github/controller/MainController.java
+++ src/main/java/com/github/controller/MainController.java
@@ -174,10 +174,15 @@ public class MainController {
     private void actionRemoveSelectedFilesFromTable() {
         remove_files_button.setOnAction(event -> {
             // https://coderoad.ru/52449706/JavaFX-%D1%83%D0%B4%D0%B0%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B8%D0%B7-TableView
-            logger.debug("Содержимое taskList до нажатия кнопки \"Удалить файлы\"" + list);
-            task_table.getItems().removeAll(List.copyOf(task_table.getSelectionModel().getSelectedItems()));
-            logger.debug("Содержимое taskList после нажатия кнопки \"Удалить файлы\": " + list);
-            task_table.refresh();
+            if (task_table.getSelectionModel().getSelectedItems().size() > 0) {
+                logger.debug("Содержимое taskList до нажатия кнопки \"Удалить файлы\"" + list);
+                task_table.getItems().removeAll(List.copyOf(task_table.getSelectionModel().getSelectedItems()));
+                logger.debug("Содержимое taskList после нажатия кнопки \"Удалить файлы\": " + list);
+                task_table.refresh();
+            } else {
+                logger.debug("Не выбрано ни одного файла");
+            }
+
         });
     }
 
