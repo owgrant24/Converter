be6c0fe846d8a518ffd431fd64933f24b720c21d
Aleksandr Shabelskii <owgrant18@gmail.com>
2021-10-14 19:18:40 +0300

    141021 Fix time (min and sec)

--- src/main/java/com/github/util/HelperUtil.java
+++ src/main/java/com/github/util/HelperUtil.java
@@ -16,4 +16,19 @@ public class HelperUtil {
         return stringBuilder.toString();
     }
 
+    public static String convertSecInMin(String sec) {
+        int number = Integer.parseInt(sec);
+        int min = number / 60;
+        int minOst = number % 60;
+        String result;
+        if (number < 60) {
+            result = sec + " sec";
+        } else if (minOst == 0) {
+            result = min + " min ";
+        } else {
+            result = min + " min " + minOst + " sec";
+        }
+        return result;
+    }
+
 }
